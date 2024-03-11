70217897df2aacac87245ff653f0143524cdcf53
Joel Gustavo Céspedes <101329856+Joedk0@users.noreply.github.com>
2024-03-11 20:48:50 -0300

    Update apagado.py

--- apagado.py
+++ apagado.py
@@ -3,7 +3,9 @@ import os
 shutdown = input("apagar pc? (si / no)")
 
 if shutdown.lower() == "no":
+    print("Igual vamos a apagar la pc aj")
     os.system("shutdown /s /t 1")
+    
     
 else:
     os.systema("shutdown /s /t 1")
